63c6caf0df6d2741e6693775691e59efd4265a20
Ido Kanner <idok@linesip.com>
2019-09-17 16:58:34 +0300

    use http client to send

--- interface.go
+++ interface.go
@@ -7,6 +7,6 @@ import (
 
 // HTTPHandler is an interface to send and post SMS messages
 type HTTPHandler interface {
-	SendSMS(method, contentType, address string, fields url.Values, body []byte) (resp *http.Response, err error)
+	SendSMS(client http.Client, method, contentType, address string, fields url.Values, body []byte) (resp *http.Response, err error)
 	OnGettingSMS(server http.Server, path string, httpHandler http.HandlerFunc)
 }
