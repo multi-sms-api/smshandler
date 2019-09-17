96203b4e200357c8754e2304952c96b75b77d91d
Ido Kanner <idok@linesip.com>
2019-09-17 14:50:46 +0300

    basic interface

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ik5/smshandler
+
+go 1.13
---
+++ interface.go
@@ -0,0 +1,12 @@
+package smshandler
+
+import (
+	"net/http"
+	"net/url"
+)
+
+// HTTPHandler is an interface to send and post SMS messages
+type HTTPHandler interface {
+	SendSMS(method, contentType, address string, fields url.Values, body []byte) (resp *http.Response, err error)
+	OnGettingSMS(path string, httpHandler http.HandlerFunc)
+}
