9215877ae02e6d5bc70e17d6e5009a320e7cb7a1
Ido Kanner <idok@linesip.com>
2019-10-03 17:10:52 +0300

    fixing interface issues

--- interface.go
+++ interface.go
@@ -1,10 +1,19 @@
 package smshandler
 
-import "net/http"
+import (
+	"net/http"
+	"net/url"
+)
 
 // XMLHandler is an interface to send and post SMS messages
 type XMLHandler interface {
 	SendSMS(client *http.Client) (*http.Response, error)
+}
+
+// HTTPHandler interface for working with HTTP client interface
+type HTTPHandler interface {
+	DoHTTP(method, contentType, address string, fields url.Values, body []byte) (resp *http.Response, err error)
+	OnGettingSMS(path string, mux *http.ServeMux, httpHandler http.HandlerFunc)
 }
 
 // Response holds interface for
