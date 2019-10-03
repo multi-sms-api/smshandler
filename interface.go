eee6d8d49de134095c9f1cc1cc7e051fdfe27f87
Ido Kanner <idok@linesip.com>
2019-10-03 17:07:47 +0300

    cleaned interface a bit

--- interface.go
+++ interface.go
@@ -2,17 +2,9 @@ package smshandler
 
 import "net/http"
 
-// XMLResponse holds interface for XMLResponse that are implemented on each provider
-type XMLResponse struct{}
-
-// HTTPHandler holds interface for HTTP client usage that implemented by each provider
-type HTTPHandler struct {
-	Client *http.Client
-}
-
 // XMLHandler is an interface to send and post SMS messages
 type XMLHandler interface {
-	SendSMS(h HTTPHandler) (*http.Response, error)
+	SendSMS(client *http.Client) (*http.Response, error)
 }
 
 // Response holds interface for
