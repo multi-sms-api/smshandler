8b72f8bc3824ccf0eef737fdb29d29e57c880efa
Ido Kanner <idok@linesip.com>
2019-10-20 13:25:06 +0300

    added support for both FromJSONResponse and RESTHandler

--- interface.go
+++ interface.go
@@ -10,6 +10,11 @@ type XMLHandler interface {
 	SendSMS(client *http.Client) (*http.Response, error)
 }
 
+// RESTHandler is an interface to send and post SMS messages using HTTP REST
+type RESTHandler interface {
+	SendSMS(method string, client *http.Client) (*http.Response, error)
+}
+
 // HTTPHandler interface for working with HTTP client interface
 type HTTPHandler interface {
 	DoHTTP(method, contentType, address string, fields url.Values, body []byte) (resp *http.Response, err error)
@@ -19,6 +24,7 @@ type HTTPHandler interface {
 // Response holds interface for
 type Response interface {
 	FromXMLResponse(status []byte) error
+	FromJSONResponse(status []byte) error
 	ToError() error
 	IsOK() bool
 }
